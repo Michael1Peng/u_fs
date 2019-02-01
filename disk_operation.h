babe2e920b8be89949fbe2639466f37df1079bc2
michael <625026338@qq.com>
2019-02-01 10:46:15 +0800

    finish getattr, finish finding the file and directory

--- disk_operation.h
+++ disk_operation.h
@@ -2,36 +2,35 @@
 #define disk_operation_H
 
 static long u_fs_find_directory(char *directoryname) {
-    FILE *disk = fopen(".disk", "rb");
+    FILE *disk = fopen(".disk", "rb+");
     if (disk == NULL) {
         printf("fail to open disk.\n");
         return -1;
     }
 
-    struct Sb *sb;
+    struct Sb sb;
 
-    int read_disk = (int) fread((void *) sb, sizeof(struct Sb), 1, disk);
+    int read_disk = (int) fread((void *) &sb, sizeof(struct Sb), 1, disk);
     if (read_disk <= 0) {
         printf("fail to read the sb.\n");
         return -1;
     }
-
-    fseek(disk, sb->first_blk * BLOCK_SIZE, SEEK_SET);
-    struct Root_directory *root_directory;
-    fread((void *) root_directory, sizeof(struct Root_directory), 1, SEEK_SET);
+    fseek(disk, sb.first_blk * BLOCK_SIZE, SEEK_SET);
+    struct Root_directory root_directory;
+    fread((void *) &root_directory, sizeof(struct Root_directory), 1, disk);
     for (int i = 0; i < MAX_DIRS_IN_ROOT; i++) {
-        if (strcmp(directoryname, root_directory->directories[i].directory_name) == 0) {
+        if (strcmp(directoryname, root_directory.directories[i].directory_name) == 0) {
             fclose(disk);
-            return root_directory->directories[i].nStartBlock;
+            return root_directory.directories[i].nStartBlock;
         }
     }
-    while (root_directory->nNextBlock != 0) {
-        fseek(disk, root_directory->nNextBlock * BLOCK_SIZE, SEEK_SET);
-        fread((void *) root_directory, sizeof(struct Root_directory), 1, SEEK_SET);
+    while (root_directory.nNextBlock != 0) {
+        fseek(disk, root_directory.nNextBlock * BLOCK_SIZE, SEEK_SET);
+        fread((void *) &root_directory, sizeof(struct Root_directory), 1, disk);
         for (int i = 0; i < MAX_DIRS_IN_ROOT; i++) {
-            if (strcmp(directoryname, root_directory->directories[i].directory_name) == 0) {
+            if (strcmp(directoryname, root_directory.directories[i].directory_name) == 0) {
                 fclose(disk);
-                return root_directory->directories[i].nStartBlock;
+                return root_directory.directories[i].nStartBlock;
             }
         }
     }
@@ -47,26 +46,26 @@ static long u_fs_find_file(long directory_pos, char *filename) {
     }
 
     fseek(disk, directory_pos * BLOCK_SIZE, SEEK_SET);
-    struct u_fs_File_directory *u_fs_file_directory;
-    fread((void *) u_fs_file_directory, sizeof(struct u_fs_File_directory), 1, disk);
-    fseek(disk, u_fs_file_directory->nStartBlock * BLOCK_SIZE, SEEK_SET);
-    struct Directory_entry *directory_entry;
-    fread((void *) directory_entry, sizeof(struct Directory_entry), 1, SEEK_SET);
+    struct u_fs_File_directory u_fs_file_directory;
+    fread((void *) &u_fs_file_directory, sizeof(struct u_fs_File_directory), 1, disk);
+    fseek(disk, u_fs_file_directory.nStartBlock * BLOCK_SIZE, SEEK_SET);
+    struct Directory_entry directory_entry;
+    fread((void *) &directory_entry, sizeof(struct Directory_entry), 1, disk);
 
     for (int i = 0; i < MAX_FILES_IN_DIRECTORY; ++i) {
-        if (strcmp(filename, directory_entry->u_fs_file_directory_list[i].fname) == 0) {
+        if (strcmp(filename, directory_entry.u_fs_file_directory_list[i].fname) == 0) {
             fclose(disk);
-            return directory_entry->u_fs_file_directory_list[i].nStartBlock;
+            return directory_entry.u_fs_file_directory_list[i].nStartBlock;
         }
     }
 
-    while (directory_entry->nNextBlock != 0) {
-        fseek(disk, directory_entry->nNextBlock * BLOCK_SIZE, SEEK_SET);
-        fread((void *) directory_entry, sizeof(struct Directory_entry), 1, SEEK_SET);
+    while (directory_entry.nNextBlock != 0) {
+        fseek(disk, directory_entry.nNextBlock * BLOCK_SIZE, SEEK_SET);
+        fread((void *) &directory_entry, sizeof(struct Directory_entry), 1, disk);
         for (int i = 0; i < MAX_FILES_IN_DIRECTORY; ++i) {
-            if (strcmp(filename, directory_entry->u_fs_file_directory_list[i].fname) == 0) {
+            if (strcmp(filename, directory_entry.u_fs_file_directory_list[i].fname) == 0) {
                 fclose(disk);
-                return directory_entry->u_fs_file_directory_list[i].nStartBlock;
+                return directory_entry.u_fs_file_directory_list[i].nStartBlock;
             }
         }
     }
--- u_fs.c
+++ u_fs.c
@@ -17,25 +17,43 @@ static int u_fs_getattr(const char *path, struct stat *stbuf,
     (void) fi;
 
     memset(stbuf, 0, sizeof(struct stat));
-    char directoryname[MAX_FILENAME+1];
-    char filename[MAX_FILENAME+1];
-    char extension[MAX_EXTENSION+1];
-    memset(directoryname,  0,MAX_FILENAME  + 1);
-    memset(filename, 0,MAX_FILENAME  + 1);
-    memset(extension,0,MAX_EXTENSION + 1);
+    char directoryname[MAX_FILENAME + 1];
+    char filename[MAX_FILENAME + 1];
+    char extension[MAX_EXTENSION + 1];
+    memset(directoryname, 0, MAX_FILENAME + 1);
+    memset(filename, 0, MAX_FILENAME + 1);
+    memset(extension, 0, MAX_EXTENSION + 1);
 
     if (strcmp(path, "/") == 0) {
         stbuf->st_mode = S_IFDIR | 0755;
         stbuf->st_nlink = 2;
         return 0;
     } else {
-        sscanf(path,"/%[^/]/%[^.].%s",directoryname,filename,extension);
-
+        sscanf(path, "/%[^/]/%[^.].%s", directoryname, filename, extension);
+        long location_directory = u_fs_find_directory(directoryname);
+        if (location_directory == 0) {
+            return -ENOENT;
+        }
+        if (filename[0] == '\0') { //no file name, and dir exists
+            stbuf->st_mode = S_IFDIR | 755;
+            stbuf->st_nlink = 2;
+            return 0;
+        } else {
+            size_t fsize = 0;
+            long location_file = u_fs_find_file(location_directory, directoryname);
+            if (location_file == 0) {
+                return -ENOENT;
+            }
+            stbuf->st_mode = S_IFREG | 0666;
+            stbuf->st_nlink = 1;
+            stbuf->st_size = fsize;
+            return 0;
+        }
     }
 }
 
 static struct fuse_operations u_fs_operations = {
-        .getattr
+        .getattr=u_fs_getattr,
 };
 
 int main(int argc, char *argv[]) {
